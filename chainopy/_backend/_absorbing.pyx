b5acc77d7db356786ee27d116c5069f62503ed0a
Aadya Chinubhai <77720426+aadya940@users.noreply.github.com>
2024-02-09 18:12:40 +0530

    Remove un-necessary comments

--- chainopy/_backend/_absorbing.pyx
+++ chainopy/_backend/_absorbing.pyx
@@ -16,16 +16,3 @@ def _absorbing_states_indices(double[:, :] tpm):
                 with cython.gil:
                     absorbing_states.append(state_idx)
     return absorbing_states
-
-
-# @cython.wraparound(False)
-# @cython.boundscheck(False)
-# def is_absorbing(double[:, :] tpm, list absorbing_states_indices, list absorbing_states_ ,int num_states):
-#     cdef Py_ssize_t state_idx
-#     cdef Py_ssize_t state
-#     for state_idx in range(len(absorbing_states_)):
-#         if all(
-#             tpm[state, absorbing_states_indices[state_idx]] > 0
-#                 for state in range(num_states)):
-#             return True
-#     return False
